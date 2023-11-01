078d145e381f6e1891d914842c2e58352e1f7c50
JodyGs <demajn@tuta.io>
2023-11-01 15:42:24 +0100

    init_

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,29 @@
+import clsx from "clsx";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="stylesheet" href="https://use.typekit.net/lob0eet.css" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html>
+			<Head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin="true"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<body
+				className={clsx({
+					"debug-screens": process.env.NODE_ENV === "development",
+				})}
+			>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,49 @@
 import Header from "@/components/Header";
-import Hero from "@/components/sections/Hero";
-import About from "@/components/sections/About";
-import Philosophy from "@/components/sections/Philosophy";
-import Games from "@/components/sections/Games";
-import Team from "@/components/sections/Team";
-import LastUpdates, { RSSFeed } from "@/components/sections/LastUpdates";
+import { RSSFeed } from "@/components/sections/LastUpdates";
 import Footer from "@/components/Footer";
 import { Meta } from "@/layout/Meta";
+import OurServices from "@/components/sections/OurServices";
+import HeroBanner from "@/components/sections/HeroBanner";
+import WhoAreWe from "@/components/sections/WhoAreWe";
+import DNAStatement from "@/components/sections/DNAStatement";
+import JoinOurTeam from "@/components/sections/JoinOurTeam";
+
 export default function Home({ rssFeed }: { rssFeed: RSSFeed }) {
-  return (
-    <>
-      <Meta
-        title={
-          "Starchaingazer.io – Building entertaining games for everyone, everywhere."
-        }
-        description={
-          "Building the most user-centric Web3 game studio in the world, creating powerful gaming experiences."
-        }
-        canonical={"https://starchaingazer.io/"}
-      />
-      <div className="bg-container"></div>
-      <div className="m-auto max-w-10xl">
-        <Header />
-        <Hero />
-        <About id="about" />
-        <Philosophy id="philosophy" />
-        <Games id="games" />
-        <Team id="team" />
-        <LastUpdates rssFeed={rssFeed} id="news" />
-        <Footer />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Meta
+				title={
+					"Starchaingazer.io – Building entertaining games for everyone, everywhere."
+				}
+				description={
+					"Building the most user-centric Web3 game studio in the world, creating powerful gaming experiences."
+				}
+				canonical={"https://starchaingazer.io/"}
+			/>
+			<div className="m-auto max-w-10xl">
+				<Header />
+				{/* <Hero /> */}
+				<HeroBanner />
+				<OurServices />
+				<WhoAreWe />
+				<DNAStatement />
+				<JoinOurTeam />
+				<Footer />
+			</div>
+		</>
+	);
 }
 
 export async function getStaticProps() {
-  const res = await fetch(
-    "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/starchaingazer"
-  );
-  const rssFeed = (await res.json()) as RSSFeed;
+	const res = await fetch(
+		"https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/starchaingazer"
+	);
+	const rssFeed = (await res.json()) as RSSFeed;
 
-  return {
-    props: {
-      rssFeed,
-    },
-    revalidate: 86400, // 1 day
-  };
+	return {
+		props: {
+			rssFeed,
+		},
+		revalidate: 86400, // 1 day
+	};
 }
